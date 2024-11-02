b6fb751df9c4024782902c21ce5f5631bc3ff1f4
Wyrdix <wyrdix@gmail.com>
2024-11-02 20:43:16 +0100

    Customize page title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,9 @@
 	initializeStores();
 </script>
 
+<svelte:head>
+	<title>QCM</title>
+</svelte:head>
 <div class=" flex flex-col">
 	<div class="bg-surface-100-800-token flex flex-row justify-between p-5">
 		<a href="/"><h3 class="h3">QCM</h3></a>
