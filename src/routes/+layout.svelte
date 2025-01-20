d6cc36bec35619e8168bf9341f7e31463c6a5d7a
Wyrdix <wyrdix@gmail.com>
2025-01-20 18:45:00 +0100

    Add favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,10 +8,14 @@
 
 <svelte:head>
 	<title>QCM</title>
+	<!-- <link rel="icon" type="image/svg" href={'favicon.png'} /> -->
 </svelte:head>
 <div class=" flex flex-col">
 	<div class="bg-surface-100-800-token flex flex-row justify-between p-5">
-		<a href="/"><h3 class="h3">QCM</h3></a>
+		<a class="flex flex-row items-center gap-5 text-center align-middle" href="/">
+			<img src="favicon.svg" class="h-10" alt="icon" />
+			<h3 class="h3">QCM</h3>
+		</a>
 		<!-- <LightSwitch /> -->
 	</div>
 	<div class="relative">
