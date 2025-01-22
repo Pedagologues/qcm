df7f354512b9bfbe1f44ab4a0671c4bd1e672f18
Wyrdix <wyrdix@gmail.com>
2025-01-22 15:53:07 +0100

    Improve edition rendering

--- src/lib/plugin-qcm/qcm.ts
+++ src/lib/plugin-qcm/qcm.ts
@@ -2,7 +2,6 @@ import type { Plugin } from 'carta-md';
 import type { Root as HRoot } from 'hast';
 import type { Root, RootContent } from 'mdast';
 import type { Plugin as UPlugin } from 'unified';
-import { visit } from 'unist-util-visit';
 
 export type QCMExtensionOptions = {
 	view: boolean;
@@ -11,45 +10,101 @@ export type QCMExtensionOptions = {
 const REGEX = /(\n#\?#\n?)/;
 
 const qcm_plugin: UPlugin<void[], Root> = () => {
-	return (tree) => {
-		tree.children = tree.children
-			.map((node): RootContent[] => {
-				if (node.type != 'paragraph') return [node];
-				if (!node.children.find((v) => v.type == 'text' && v.value.includes('\n#?#')))
-					return [node];
-				if (node.children.find((v) => v.type == 'text' && v.value.split('\n#?#').length > 2))
-					return [node];
+	let section_creator = (
+		list: RootContent[],
+		indexer: Map<string, number>,
+		sanitizer: (v: string) => string,
+		title: string | undefined
+	): RootContent[] => {
+		if (list.length == 0) return [];
 
-				console.log(node);
+		let type = '';
+		if (JSON.stringify(list).includes('"tagName":"input"')) type = 'Question';
+		else type = 'Text';
 
-				const missingChild: RootContent[] = [];
+		indexer.set(sanitizer(type), (indexer.get(sanitizer(type)) || 0) + 1);
 
-				node.children.forEach((v, i) => {
-					if (v.type !== 'text') missingChild.push(v);
-					else {
-						const txt = v.value;
-						if (!txt.includes('\n#?#')) missingChild.push(v);
-						else {
-							txt.split('\n#?#').forEach((o, j) => {
-								if (o.trim().length !== 0) {
-									missingChild.push({
-										type: 'paragraph',
-										children: [{ type: 'text', value: o }]
-									});
-								}
-								if (o.trim().length !== 0 || j == 0)
-									missingChild.push({
-										type: 'paragraph',
-										children: [{ type: 'text', value: '===== NEW QUESTION =====' }]
-									});
-							});
+		let real_title = type + ' #' + indexer.get(sanitizer(type));
+
+		if (title) {
+			const v = indexer.get(sanitizer(title));
+			if (v) {
+				real_title = title + ' #' + v;
+			} else {
+				real_title = title;
+			}
+			indexer.set(sanitizer(title), (v || 0) + 1);
+		}
+
+		return [
+			{
+				type: 'element',
+				tagName: 'div',
+				children: [
+					{
+						type: 'element',
+						tagName: 'div',
+						children: [
+							{
+								type: 'text',
+								value: real_title
+							}
+						],
+						properties: {
+							className: ['question-section-title']
+						}
+					},
+					{
+						type: 'element',
+						tagName: 'section',
+						children: Array(...list),
+						properties: {
+							className: ['question-section-body']
 						}
 					}
-				});
+				],
+				properties: {
+					className: ['question-section']
+				}
+			}
+		] as any;
+	};
 
-				return missingChild;
+	return (tree) => {
+		let stack: RootContent[] = [];
+
+		let indexer: Map<string, number> = new Map();
+
+		let sanitizer = (v: string): string => v.replace(RegExp('\s+'), '-');
+
+		let last_title: string | undefined = undefined;
+
+		tree.children = tree.children
+			.map((node): RootContent[] => {
+				if ((node.type as string) !== 'element') {
+					stack.push(node);
+					return [];
+				}
+
+				const REGEX2 = /^(#\?#)(\s*\((.*)\))?\s*$/;
+				const children: any[] = (node as any).children;
+				if (!(children.length === 1 && REGEX2.test(children[0].value))) {
+					stack.push(node);
+					return [];
+				}
+
+				const text = children[0].value;
+
+				const newNode = section_creator(stack, indexer, sanitizer, last_title);
+
+				last_title = (REGEX2.exec(text) || [])[3] || undefined;
+
+				stack = [];
+
+				return newNode;
 			})
 			.flat();
+		tree.children.push(...section_creator(stack, indexer, sanitizer, last_title));
 	};
 };
 
@@ -58,58 +113,9 @@ export const qcm = (options?: QCMExtensionOptions): Plugin => {
 		transformers: [
 			{
 				execution: 'sync',
-				type: 'remark',
-				transform({ processor: n }): void {
-					n.use(qcm_plugin);
-				}
-			},
-			{
-				execution: 'sync',
 				type: 'rehype',
 				transform({ processor }): void {
-					if (options?.view)
-						processor.use(() => {
-							return (tree: HRoot) => {
-								let i = 0;
-								visit(tree, (node, index, parent) => {
-									if (node.type !== 'element') return;
-									if (node.tagName !== 'input') return;
-									node.properties.checked = undefined;
-									node.properties.disabled = undefined;
-									node.properties.id = 'answer_' + i;
-									i += 1;
-								});
-							};
-						});
-				}
-			}
-		],
-		grammarRules: [
-			{
-				name: 'question',
-				type: 'inline',
-				definition: {
-					match: '(^#\\?#\n+)',
-					name: 'markup.inline.question',
-					captures: {
-						'1': { name: 'marker.definition.question.inline' }
-					}
-				}
-			}
-		],
-		highlightingRules: [
-			{
-				light: {
-					scope: 'marker.definition.question',
-					settings: {
-						foreground: '#0b0'
-					}
-				},
-				dark: {
-					scope: 'marker.definition.question',
-					settings: {
-						foreground: '#4DACFA'
-					}
+					processor.use(qcm_plugin);
 				}
 			}
 		]
