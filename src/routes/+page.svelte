2852ac6e7e4dc43901db113c3e387f7276559ecb
Wyrdix <wyrdix@gmail.com>
2025-02-15 14:46:56 +0100

    Enable suppression of cached documents and answers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,44 @@
 <script lang="ts">
+	import { getModalStore, type ModalSettings } from '@skeletonlabs/skeleton';
 	import Menu from '../lib/components/Menu.svelte';
 	import { cached_documents, cached_answers } from '../store';
+
+	const modalStore = getModalStore();
+	let write_suppression: string | undefined = undefined;
+	const write_suppression_modal: ModalSettings = {
+		type: 'confirm',
+		// Data
+		title: 'Please Confirm',
+		body: 'Are you sure you wish to proceed?',
+		// TRUE if confirm pressed, FALSE if cancel pressed
+		response: (r: boolean) => {
+			if (write_suppression !== undefined && r) {
+				cached_documents.update((v) => {
+					delete v[write_suppression as string];
+					return v;
+				});
+			}
+			write_suppression = undefined;
+		}
+	};
+
+	let answer_suppression: string | undefined = undefined;
+	const answer_suppression_modal: ModalSettings = {
+		type: 'confirm',
+		// Data
+		title: 'Please Confirm',
+		body: 'Are you sure you wish to proceed?',
+		// TRUE if confirm pressed, FALSE if cancel pressed
+		response: (r: boolean) => {
+			if (answer_suppression !== undefined && r) {
+				cached_answers.update((v) => {
+					delete v[answer_suppression as string];
+					return v;
+				});
+			}
+			answer_suppression = undefined;
+		}
+	};
 </script>
 
 <div
@@ -15,16 +53,28 @@
 			</div>
 
 			<div class="mx-2 my-4 flex w-full flex-1 flex-col">
-				<ul class="list flex-1 mr-10">
+				<ul class="list mr-10 flex-1">
 					{#each Object.keys($cached_documents) as doc}
 						<li class="list-item">
-							<a class="flex-1 p-2 rounded-lg flex hover:bg-surface-200" target="_blank" href={"/edit/"+doc}>
-								<div class="flex flex-1 flex-row justify-center">
+							<div class="flex flex-1 flex-row justify-center">
+								<a
+									class="hover:bg-surface-200 flex flex-1 rounded-lg p-2"
+									target="_blank"
+									href={'/edit/' + doc}
+								>
 									<div>{$cached_documents[doc].title}</div>
 									<div class="flex-1"></div>
 									<div>Edited : {new Date($cached_documents[doc].updated).toLocaleString()}</div>
-								</div>
-							</a>
+								</a>
+								<div class="min-w-5"></div>
+								<button
+									class="text-red-500 hover:bg-red-900"
+									on:click={() => {
+										write_suppression = doc;
+										modalStore.trigger(write_suppression_modal);
+									}}>X</button
+								>
+							</div>
 						</li>
 					{/each}
 				</ul>
@@ -43,19 +93,31 @@
 			</div>
 
 			<div class="mx-2 my-4 flex w-full flex-1 flex-col">
-				<ul class="list flex-1 mr-10">
+				<ul class="list mr-10 flex-1">
 					{#each Object.keys($cached_answers) as doc}
 						<li class="list-item">
-							<a class="flex-1 p-2 rounded-lg flex hover:bg-surface-200" target="_blank" href={"/view/"+doc}>
-								<div class="flex flex-1 flex-col justify-center">
-									<div class="flex flex-1 flex-row">
+							<div class="flex flex-1 flex-col justify-center">
+								<div class="flex flex-1 flex-row">
+									<a
+										class="hover:bg-surface-200 flex flex-1 rounded-lg p-2"
+										target="_blank"
+										href={'/view/' + doc}
+									>
 										<div>{$cached_answers[doc].title}</div>
 										<div class="flex-1"></div>
-										<div>Edited : {new Date($cached_answers[doc].updated).toLocaleString()}</div>7
-									</div>
-									Access : {doc}
+										<div>Edited : {new Date($cached_answers[doc].updated).toLocaleString()}</div>
+									</a>
+									<div class="min-w-5"></div>
+									<button
+										class="text-red-500 hover:bg-red-900"
+										on:click={() => {
+											answer_suppression = doc;
+											modalStore.trigger(answer_suppression_modal);
+										}}>X</button
+									>
 								</div>
-							</a>
+								Access : {doc}
+							</div>
 						</li>
 					{/each}
 				</ul>
